9eec4c96af53078268c5afdc376615904bc1831b
paulao255 <contactpaulao@gmail.com>
2026-01-17 00:53:24 -0300

    Update PG-C to 2026/01/17 in main branch.

--- src/Main.c
+++ src/Main.c
@@ -1,36 +1,40 @@
 /* Include guard: */
-#ifndef PASSWORD_GENERATOR_C
-#define PASSWORD_GENERATOR_C
+#ifndef PG_C
+#define PG_C
 
 /* Importations: */
-#include <cutils.h>
+#include "C-Utils/cutils.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 
+/* PG-C version variables: */
+#define PG_C_FULL_VERSION       20260117 /* PG-C full version variable (2026/01/17). */
+#define PG_C_MAJOR_VERSION      2026     /* PG-C major version variable (2026).      */
+#define PG_C_MINOR_VERSION      01       /* PG-C minor version variable (01).        */
+#define PG_C_PATCH_VERSION      17       /* PG-C patch version variable (17).        */
+
+/* Defines: */
+#define MINIMUM_PASSWORD_LENGHT 3        /* Minimum password lenght variable.        */
+#define MAXIMUM_PASSWORD_LENGHT 20       /* Maximum password lenght variable.        */
+
 /* Main code: */
-int main()
+int main(void)
 {
-	/* Password Generator version variables: */
-	#define PASSWORD_GENERATOR_FULL_VERSION  100.0L                                                                           /* Password Generator full version variable.                          */
-	#define PASSWORD_GENERATOR_MAJOR_VERSION   1.0L                                                                           /* Password Generator major version variable.                         */
-	#define PASSWORD_GENERATOR_MINOR_VERSION   0.0L                                                                           /* Password Generator minor version variable.                         */
-	#define PASSWORD_GENERATOR_PATCH_VERSION   0.0L                                                                           /* Password Generator patch version variable.                         */
-
-	/* Start seed: */
-	srand((unsigned)time(NULL));
-
 	/* Local variables: */
-	#define MINIMUM_PASSWORD_LENGHT            3                                                                              /* Minimum password lenght variable.                                  */
-	#define MAXIMUM_PASSWORD_LENGHT           20                                                                              /* Maximum password lenght variable.                                  */
 	int number = 0;                                                                                                           /* Ammount of characters in the password variable.                    */
 	int times = 0;                                                                                                            /* Times to generate random characters for the password.              */
 	char snumber[8192] = "";                                                                                                  /* String number variable.                                            */
 	char characters[95] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-=_+!@#$%^&*()[]{};:'\"\\|/?,.<>`~"; /* Variable to store all the possible characters to be in a password. */
 	char password[21] = "";                                                                                                   /* Variable to store the generated password.                          */
 
+	/* Start seed: */
+	srand((unsigned) time(NULL));
+
+	/* Main loop: */
 	while(number < MINIMUM_PASSWORD_LENGHT || number > MAXIMUM_PASSWORD_LENGHT)
 	{
+		clear_terminal();
 		printf("Type the ammount of digits that you want to have in the password (Minimum: %d; Maximum: %d): ", MINIMUM_PASSWORD_LENGHT, MAXIMUM_PASSWORD_LENGHT);
 		scanf("%8191s", snumber);
 		number = atoi(snumber);
